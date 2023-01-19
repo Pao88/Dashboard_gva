9d81b415cd7b20d4eb0faed1a930475c35404d12
Paolino De Falco <52710199+Pao88@users.noreply.github.com>
2023-01-19 16:03:42 +0100

    Update first_prot.py

--- first_prot.py
+++ first_prot.py
@@ -13,25 +13,15 @@ import datetime as dt
 import flask
 import dash_bootstrap_components as dbc
 
-'''
-data_GVA = pd.read_excel('GVA Data - Delays prediction - Winter 20222023-01-09.xlsx','InnoHUB - TIMINGS')
-data_GVA = data_GVA.rename(columns={'Date':'date_dt','Call Sign - IATA': 'callsign','Arrival - Departure Code':'code'})
-df = pd.read_csv('predictions_on_2022-12-26_from_2022-12-27-to-2023-03-25.csv', delimiter=';')
-df['date_dt'] = df.apply(lambda x: dt.datetime.strptime(x.date, '%Y-%m-%d'), axis =1)
 
-df= df.merge(data_GVA[['date_dt','callsign','code','UTC Schedule Time']], on = ['date_dt','callsign','code'], how='left')
-df.to_csv('dataset_dashboard.csv')
-'''
 df = pd.read_csv('dataset_dashboard.csv', delimiter=',')
 df =df[df['code']=='D']
 df['diff'] = df['0.75'] - df['0.25']
 app = Dash(__name__)
-#server = flask.Flask(__name__)
 dbc_css = ("https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css")
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
 server = app.server
-#app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.ZEPHYR,dbc_css])
-#app.title = 'TVInteractions'
+
 
 colors = {
     'background': '#111111',
