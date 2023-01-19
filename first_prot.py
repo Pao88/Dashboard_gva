eb004c3f3dfec7a759c0915cd243d665dc6c728b
Paolino De Falco <52710199+Pao88@users.noreply.github.com>
2023-01-19 16:14:57 +0100

    Add files via upload

--- first_prot.py
+++ first_prot.py
@@ -9,26 +9,34 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
-import datetime as dt
-import flask
-import dash_bootstrap_components as dbc
 
+#import dash_bootstrap_components as dbc
 
+'''
+data_GVA = pd.read_excel('GVA Data - Delays prediction - Winter 20222023-01-09.xlsx','InnoHUB - TIMINGS')
+data_GVA = data_GVA.rename(columns={'Date':'date_dt','Call Sign - IATA': 'callsign','Arrival - Departure Code':'code'})
+df = pd.read_csv('predictions_on_2022-12-26_from_2022-12-27-to-2023-03-25.csv', delimiter=';')
+df['date_dt'] = df.apply(lambda x: dt.datetime.strptime(x.date, '%Y-%m-%d'), axis =1)
+
+df= df.merge(data_GVA[['date_dt','callsign','code','UTC Schedule Time']], on = ['date_dt','callsign','code'], how='left')
+df.to_csv('dataset_dashboard.csv')
+'''
 df = pd.read_csv('dataset_dashboard.csv', delimiter=',')
 df =df[df['code']=='D']
 df['diff'] = df['0.75'] - df['0.25']
 app = Dash(__name__)
-dbc_css = ("https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css")
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
-server = app.server
-
+#server = flask.Flask(__name__)
+#dbc_css = ("https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.2/dbc.min.css")
+#app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css])
+#app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.ZEPHYR,dbc_css])
+#app.title = 'TVInteractions'
 
 colors = {
     'background': '#111111',
     'text': '#7FDBFF'}
 
 
-app.layout = dbc.Container(html.Div(children=[
+app.layout = html.Div(children=[
     html.H1(children='Predicted Departure Delays at Geneve Airport'),
     dcc.Dropdown(id='dates',
                  options=[{'label':i, 'value':i } for i in df['date'].unique()] ),
@@ -46,9 +54,7 @@ app.layout = dbc.Container(html.Div(children=[
         id='Difference', 
         ),    
 
-]),
-    fluid=True,
-    className="dbc"
+]
 )
 
              
